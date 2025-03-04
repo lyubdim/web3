ecf29aee94550df160db0bbc4f01775be32e415c
belozerov-yaroslav <jarblz@yandex.ru>
2025-03-04 17:56:15 +0500

    2nd task

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            let todos = getFiles();
+            res = [];
+            for (let file_string of todos){
+                res.push(file_string.split('\r\n').filter((word) => word.trim().startsWith("// TODO")));
+            }
+            console.log(res);
+            process.exit(0);
+            break;
         default:
             console.log('wrong command');
             break;
